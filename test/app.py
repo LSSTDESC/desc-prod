cc648c55ed804cb32b708f68652ee1a23fc1257d
David Adams <dladams@bnl.gov>
2022-11-07 12:29:56 -0500

    Update test

--- test/app.py
+++ test/app.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 class Msg:
     msg = ''
 
+@app.route("/help")
+def bye():
+    Msg.msg = '<H1>Hello help</H1>'
+    Msg.msg += '<br> help - This message.'
+    Msg.msg += '<br> bye - Clears page.'
+    Msg.msg += '<br> hello/John - Says hello to John'
+    Msg.msg += '<br> hello/John - Says hello to John'
+    Msg.msg += '<br> hello/John?Doe - Says hello to John Doe'
+    return ""
+
 @app.route("/bye")
 def bye():
     Msg.msg = ''
