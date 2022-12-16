0516e153e6aa6944d2708b0752c24216c4966474
David Adams <dladams@bnl.gov>
2022-12-16 14:15:39 -0500

    Fix.

--- test/app.py
+++ test/app.py
@@ -166,7 +166,7 @@ def home():
 def login():
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
-    redirect_uri = request.base_url + "/callback"
+    redirect_uri = base_url(request) + "/callback"
     # For anything but local host, make sure redirect is https.
     if redirect_uri[0:5] == 'http:' and redirect_uri.find('localhost') < 0 and redirect_uri.find('127.0.0.1') < 0:
         redirect_uri = redirect_uri.replace('http:', 'https:')
