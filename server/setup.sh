3c26db824e9c32c26d3f1621252805e99d490d44
David Adams <dladams@bnl.gov>
2023-01-06 16:33:46 -0500

    Fix setup.sh

--- server/setup.sh
+++ server/setup.sh
@@ -3,7 +3,7 @@
 
 # This file is sourced before the server is started.
 
-SUPS=$(ls $HOME/local/etc/setup_*.sh 2>/dev/null)"
+SUPS="$(ls $HOME/local/etc/setup_*.sh 2>/dev/null)"
 if [ -n "$SUPS" ]; then
   for FIL in $SUPS; do
     echo Sourcing $FIL
