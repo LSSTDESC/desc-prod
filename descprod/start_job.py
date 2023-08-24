1b0380b7742c2b3da2f172792d54aeae1dc1bf51
David Adams <dladams@bnl.gov>
2023-08-24 12:32:26 -0700

    Add follow to jos start. 0.4.14 #4

--- descprod/start_job.py
+++ descprod/start_job.py
@@ -5,6 +5,7 @@ import os
 import descprod
 import pdb
 import subprocess
+import time
 
 def _runcom(com):
     return subprocess.run(com, stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
@@ -46,6 +47,8 @@ def start_job_main():
     do_help = False
     debug = False
     wait = False
+    follow = False
+    fopt = ""
     myname = os.path.basename(sys.argv[0])
     while len(args) and args[0][0] == '-':
         flag = args[0]
@@ -53,9 +56,12 @@ def start_job_main():
         if flag == '-h': do_help = True
         elif flag == '-d': debug = True
         elif flag == '-w': wait = True
+        elif flag == '-f': fopt = "job"
+        elif flag == '-p': fopt = "progress"
         else:
             print(f"{myname}: Invalid command line flag: {flag}")
             return 1
+    follow = fopt == "job" or fopt == "progress"
     if len(args):
         sjid = args[0]
         uid = args[1] if len(args) > 1 else None
@@ -67,6 +73,8 @@ def start_job_main():
         print(f"     -h - Show this message and exit.")
         print(f"     -d - Run with debugger.")
         print(f"     -w - Wait for job to complete before exiting.")
+        print(f"     -f - Follow job log.")
+        print(f"     -p - Follow progress log.")
         print(f"  JOBID - Job ID.")
         print(f"   USER - DESC username.")
         print(f"    URL - server URL [{descprod.server_url()}]")
@@ -85,17 +93,48 @@ def start_job_main():
         return 1
     job = resp
     jdat = job.jmap()
+    jlog = resp.log_file()
     print(f"{myname}: Started job {jid}:")
     for key in descprod.JobData.data_names:
         if key in jdat:
             val = jdat[key]
             if key[-5:] == '_time':
                 val = f"{descprod.sdate(val)} UTC"
-            print(f"{key:>16}: {val}")
-    if wait:
-        print(f"{myname}: Waiting for process {job.popen().pid} to complete.")
-        timeout = None
-        job.popen().wait(timeout)
-        rc = job.popen().returncode
-        print(f"{myname}: Process return code: {rc}")
+            print(f"{myname}:{key:>16}: {val}")
+    pid = job.popen().pid
+    fproc = None
+    fline = '========================================================================'
+    if follow:
+        flog = jlog
+        if fopt == "progress":
+            flog = f"{os.path.dirname(flog)}/progress.log"
+        count = 0
+        # Wait for log file to appear.
+        while not os.path.exists(flog) and count < 10:
+            time.sleep(1)
+            print(f"{myname}: Waiting for {flog}")
+            count += 1
+        com = f"tail -f {flog}"
+        print(f"{myname}: Following {fopt} log file. Exit with ^C.")
+        print(f"{myname}: {fline}")
+        fproc = subprocess.Popen(com, shell=True)
+        fpid = fproc.pid
+        if fpid is None:
+            print(f"{myname}:  Unable to follow log file: {flog}")
+        else:
+            if debug: print(f"{myname}: Follow process is {fpid}")
+    if wait or follow:
+        if wait: print(f"{myname}: Waiting for process {pid} to complete.")
+        while True:
+            rc = job.popen().poll()
+            if rc is not None:
+                if fproc is not None:
+                    print(f"{myname}: {fline}")
+                    print(f"{myname}: Terminating log follow.")
+                    fproc.kill()
+                    fproc.wait()
+                print(f"{myname}: Job process {pid} returned code: {rc}")
+                break
+            if wait: print(f"{myname}: Still Waiting for process {pid}")
+            time.sleep(10)
     return 0
